ab8d1d08ec516795f2219a3f0cb427036234ada9
grysngrhm-tech <grysngrhm@gmail.com>
2025-12-03 22:00:21 -0800

    admin panel bug fix

--- viewer/sw.js
+++ viewer/sw.js
@@ -3,7 +3,7 @@
  * Provides offline caching with smart caching strategies
  */
 
-const CACHE_NAME = 'grace-bible-v15';
+const CACHE_NAME = 'grace-bible-v16';
 
 // App shell files to cache on install
 const APP_SHELL = [
