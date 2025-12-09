07d548ade4ea393b62f6fadba6b0c22d17d505d2
grysngrhm-tech <grysngrhm@gmail.com>
2025-12-09 04:09:25 -0800

    settings menu bug

--- viewer/sw.js
+++ viewer/sw.js
@@ -3,7 +3,7 @@
  * Provides offline caching with smart caching strategies
  */
 
-const CACHE_NAME = 'graham-bible-v18'; // Settings + prefetch improvements
+const CACHE_NAME = 'graham-bible-v19'; // Fix missing script tag
 const IMAGE_CACHE = 'graham-bible-images-v1';
 
 // App shell files to cache on install
